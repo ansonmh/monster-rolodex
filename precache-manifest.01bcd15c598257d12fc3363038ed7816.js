0eef1fa6e99ac2fa531a1fcadc1d8e91f7638535
ansonmh <ansonmh@gmail.com>
2020-07-16 13:17:46 +0800

    Updates

---
+++ precache-manifest.01bcd15c598257d12fc3363038ed7816.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1829dce868b794605b8502427f95471c",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "fa32412a65fabdc8aa1c",
+    "url": "/monster-rolodex/static/css/main.20b4457f.chunk.css"
+  },
+  {
+    "revision": "0105f5fa7a131123a102",
+    "url": "/monster-rolodex/static/js/2.8a77c072.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.8a77c072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa32412a65fabdc8aa1c",
+    "url": "/monster-rolodex/static/js/main.05f59b8b.chunk.js"
+  },
+  {
+    "revision": "753b30f80137f55e537c",
+    "url": "/monster-rolodex/static/js/runtime-main.e7a3ea5b.js"
+  }
+]);
